35d374b34ce896be798a18afc7af2875bef0746f
sungbin5304 <sungbin.dev@gmail.com>
2021-01-07 19:20:46 +0900

    중간 백업

--- app/src/main/java/me/sungbin/counter/MainActivity.kt
+++ app/src/main/java/me/sungbin/counter/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.sungbin.androidutils.util.ToastLength
+import com.sungbin.androidutils.util.ToastType
+import com.sungbin.androidutils.util.ToastUtil
 import me.sungbin.counter.databinding.ActivityMainBinding
 import me.sungbin.counter.databinding.LayoutDialogAddCounterBinding
+import me.sungbin.counter.databinding.LayoutDialogRandomChooseBinding
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +32,19 @@ class MainActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         }
 
+        binding.fabTools.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle(getString(R.string.main_tool))
+            dialog.setItems(arrayOf("랜덤 숫자 뽑기", "[추후 더 추가됩니다]")) { _, index ->
+                when (index) {
+                    0 -> {
+                        showRandomGameDialog()
+                    }
+                }
+            }
+            dialog.show()
+        }
+
         binding.fabAdd.setOnClickListener {
             val layout = LayoutDialogAddCounterBinding.inflate(layoutInflater)
             val dialog = AlertDialog.Builder(this)
@@ -43,4 +61,38 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun showRandomGameDialog() {
+        val layout = LayoutDialogRandomChooseBinding.inflate(layoutInflater)
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle(getString(R.string.main_random_choose))
+        dialog.setView(layout.root)
+        dialog.setPositiveButton(getString(R.string.create)) { _, _ ->
+            val maxIndex = layout.etMaxIndex.text.toString()
+            val chooseCount = layout.etChooseCount.text.toString()
+            if (maxIndex.isBlank() || chooseCount.isBlank()) {
+                ToastUtil.show(
+                    applicationContext,
+                    getString(R.string.dialog_confirm_input_all),
+                    ToastLength.SHORT,
+                    ToastType.WARNING
+                )
+            } else {
+                val chosenIndex = ArrayList<Int>()
+                while (true) {
+                    if (chosenIndex.size == chooseCount.toInt()) {
+                        break
+                    }
+                    val randomIndex = Random.nextInt(maxIndex.toInt() + 1)
+                    if (!chosenIndex.contains(randomIndex)) {
+                        chosenIndex.add(randomIndex)
+                    }
+                }
+                AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.dialog_chosen_result_title))
+                    .setMessage()
+            }
+        }
+    }
+
 }
